50f04ad94d61b3022c29e04b9c5ecb0db6ab7a7c
apiwemacingwane <103777597+apiwemacingwane@users.noreply.github.com>
2022-10-30 21:09:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = process.env.PORT||4000;
 
 // CONNECT DATABASE
 
-mongoose.connect('mongodb://localhost:27017/crime', {
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/crimetrackerapp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => console.log('DB connection successful'));
